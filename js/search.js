889387fbd44071d35d8731ea4f181f4eec78304b
Dave Smith <dasmithm@amazon.com>
2020-06-08 18:23:20 -0600

    Add highlighting in the search results and format the markdown (with showdown)
    Also remove some duplicated code

--- js/search.js
+++ js/search.js
@@ -1,19 +1,12 @@
 (function() {
-  var idx = createSearchIndex();
-  window.searchFormSubmitted = searchFormSubmitted;
-  window.addEventListener('popstate', urlChanged);
+  var BASE_URL = '/search/'
+  var idx = createSearchIndex()
+  window.searchFormSubmitted = searchFormSubmitted
+  window.addEventListener('popstate', urlChanged)
   urlChanged()
-  /*
-  var searchTerm = getQueryVariable('q');
-  if (searchTerm) {
-    document.getElementById('search-box').setAttribute("value", searchTerm)
-    doSearch(searchTerm)
-  }
-  */
 
   function urlChanged() {
-    console.log("urlChanged:", window.location.pathname + window.location.search)
-    var searchTerm = getQueryVariable('q');
+    var searchTerm = getQueryVariable('q')
     doSearch(searchTerm)
   }
 
@@ -25,90 +18,128 @@
 
   function createSearchIndex() {
     return lunr(function () {
-      this.field('id');
-      this.field('title', { boost: 10 });
-      this.field('author');
-      this.field('category');
-      this.field('content');
+      this.field('id')
+      this.field('title', { boost: 10 })
+      this.field('author')
+      this.field('category')
+      this.field('content')
+      this.metadataWhitelist.push('position')
 
-      for (var key in window.postStore) { // Add the data to lunr
-        var post = window.postStore[key];
+      for (var key in window.episodeStore) { // Add the data to lunr
+        var post = window.episodeStore[key]
         this.add({
           'id': key,
           'title': post.title,
           'author': post.author,
           'category': post.category,
           'content': post.content
-        }, this);
+        }, this)
       }
-    });
+    })
   }
 
   function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
+    var query = window.location.search.substring(1)
+    var vars = query.split('&')
   
     for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
+      var pair = vars[i].split('=')
   
       if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'))
       }
     }
   }
   
-  function displaySearchResults(results, postStore) {
-    console.log("displaySearchResults", results.length)
-    var resultList = document.getElementById('search-results');
+  function displaySearchResults(episodes, episodeStore) {
+    var summaryLabel = document.getElementById('search-results-summary')
+    var resultList = document.getElementById('search-results')
+  
+    var summaryHtml = 'No results found'
+    var listHtml = ''
 
-    document.getElementById('search-results-summary').innerHTML = 
-        results.length > 0
-          ? 'Found ' + results.length + ' episodes:'
-          : ''
-  
-    var resultsHtml = '<li>No results found</li>'
-    if (results.length > 0) {
-      resultsHtml = ''
-      for (var i=0; i<results.length; i++) {
-        var post = postStore[results[i].ref];
-        resultsHtml += '<li>'
-        resultsHtml += '  <a href="' + post.url + '"><h3>' + post.title + '</h3></a>'
-        resultsHtml += '  <p>' + post.content + '</p>'
-        resultsHtml += '</li>'
+    var converter = new showdown.Converter();
+    if (episodes.length > 0) {
+      summaryHtml = 'Found ' + episodes.length + ' episode' + (episodes.length > 1 ? 's' : '') + ':'
+      for (var i=0; i<episodes.length; i++) {
+        var episode = episodes[i]
+        listHtml += '<li>'
+        listHtml += '  <a href="' + episode.url + '"><h3>' + episode.title + '</h3></a>'
+        listHtml += '  <p>' + converter.makeHtml(episode.highlightedContent) + '</p>'
+        listHtml += '</li>'
       }
     }
   
-    resultList.innerHTML = resultsHtml;
+    summaryLabel.innerHTML = summaryHtml
+    resultList.innerHTML = listHtml
   }
   
-  function doSearch(searchTerm) {
-    if (!searchTerm || !searchTerm.trim()) {
-      searchTerm = ''
+  function doSearch(originalSearch) {
+    if (!originalSearch || !originalSearch.trim()) {
+      originalSearch = ''
     }
 
     var searchBox = document.getElementById('search-box')
-    searchBox.value = searchTerm
+    searchBox.value = originalSearch
     searchBox.blur()
 
-    if (searchTerm) {
-      var results = idx.search(searchTerm)
-      updateQueryString(searchTerm)
-      displaySearchResults(results, window.postStore)
-    } else {
-      document.getElementById('search-results').innerHTML = ''
-      document.getElementById('search-results-summary').innerHTML = ''
+    if (originalSearch) {
+      var lunrSearchString = createLunrSearchTerms(originalSearch)
+      var searchResults = idx.search(lunrSearchString)
+      var episodes = searchResults.map(function(result) {
+        var episode = episodeStore[result.ref]
+        Object.keys(result.matchData.metadata).forEach(function(term) {
+          var contentMetadata = result.matchData.metadata[term].content
+          if (contentMetadata && contentMetadata.position) {
+            var positions = contentMetadata.position
+            episode.highlightedContent = createHighlightedContent(episode.content, positions)
+          }
+        })
+        return episode
+      })
+
+      updateQueryString(originalSearch)
+      displaySearchResults(episodes, window.episodeStore)
     }
+  }
+
+  function createHighlightedContent(content, positions) {
+    var highlightedContent = content;
+    for (var i=positions.length-1; i>=0; i--) {
+      var position = positions[i]
+      var start = position[0]
+      var end = start + position[1]
+      highlightedContent = 
+        highlightedContent.slice(0, start) + 
+        '<span class="search-result-highlight">' +
+        highlightedContent.slice(start, end) + 
+        '</span> ' +
+        highlightedContent.slice(end+1, highlightedContent.length)
+    }
+    return highlightedContent
+  }
+
+  function createLunrSearchTerms(originalSearch) {
+    // Prefix all terms with "+" to perform a logical AND (unless they already have a prefix):
+    return originalSearch
+      .trim()
+      .split(/\s+/)
+      .map(function(term) {
+        if (term.startsWith('+') || term.startsWith('~')) {
+          return term
+        } else {
+          return '+' + term
+        }
+      })
+      .join(' ')
   }
 
   function updateQueryString(searchTerm) {
     var currentUrl = window.location.pathname + window.location.search
-    var newUrl = "/search/?q=" + searchTerm
+    var newUrl = BASE_URL + '?q=' + searchTerm
     if (newUrl != currentUrl) {
-      console.log("Updating history")
-      history.pushState({}, null, newUrl);
-    } else {
-      console.log("Not updating history")
+      history.pushState({}, null, newUrl)
     }
   }
 
-})();
+})()
